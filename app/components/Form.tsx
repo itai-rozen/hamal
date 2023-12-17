d92eaf646319471341575d5e39dfc40f03fe88ab
itai rozen <itai.rozen@mail.huji.ac.il>
2023-12-17 21:57:33 +0200

    hashmap for form tables

--- app/components/Form.tsx
+++ app/components/Form.tsx
@@ -1,19 +1,17 @@
-import { ChangeEvent } from "react"
+"use client"
 
-export default function Form({setUsername, setPassword}:{setUsername:Function, setPassword:Function}) {
-  const showInputs = (e: React.SyntheticEvent) => {
-    e.preventDefault()
-    console.log(e.target)
+import Equipment from "./FormInputs/Equipment"
+
+interface formProps {
+  tableName: string
+}
+export default function Form({ tableName  }: formProps)  {
+  interface inputsMap {
+    equipment : React.Component
   }
-  return (
-    <div className="form-container">
-      <form onSubmit={showInputs}>
-        <label htmlFor="username">Username</label>
-        <input type="text" id="username" name="username" onChange={(e:React.ChangeEvent<HTMLInputElement>) => setUsername(e.target.value)} />
-        <label htmlFor="password">Password</label>
-        <input type="password" name="password" id="password" onChange={(e:React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)} />
-        <input type="submit" value="Submit" />
-      </form>
-    </div>
-  )
+  const inputsMap =  {
+    equipment : <Equipment />
+  }
+  return inputsMap[tableName];
+  
 }
