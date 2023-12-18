31f1a4c209db28b07798984934191e69ba04575b
itai rozen <itai.rozen@mail.huji.ac.il>
2023-12-18 22:57:07 +0200

    commented faulty type

--- app/actions.ts
+++ app/actions.ts
@@ -2,5 +2,5 @@ import { sql } from '@vercel/postgres'
 
 export async function createQuery(formData: FormData) {
   console.log('form data: ', formData)
-  console.log('detailed form data: ',[...formData.keys()])
+  // console.log('detailed form data: ',[...formData.keys()])
 }
