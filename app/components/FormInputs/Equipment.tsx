8f6647f9f8f3c98bebf79964dd05c204e2ef0840
itai rozen <itai.rozen@mail.huji.ac.il>
2023-12-18 23:16:53 +0200

    added fields to form

--- app/components/FormInputs/Equipment.tsx
+++ app/components/FormInputs/Equipment.tsx
@@ -1,10 +1,25 @@
+import { ChangeEvent, useState } from "react"
+
 export default function Equipment() {
-  return <>
-  <label htmlFor="fullname">Full Name:</label>
-  <input id="fullname" type="text" required />
-  <label htmlFor="phone">Phone:</label>
-  <input id="phone" type="phone" required />
-  <label htmlFor="equipment-content">Equipment:</label>
-  <textarea name="equipment-content" id="equipment-content" cols={30} rows={10} placeholder="10 XL t-shirts"></textarea>
-  </>
+  const [needTransport, setNeedTransport] = useState<boolean>(false)
+  return (
+  <>
+    <label htmlFor="fullname">Full Name:</label>
+    <input id="fullname" type="text" required />
+    <label htmlFor="phone">Phone:</label>
+    <input id="phone" type="phone" required />
+    <label htmlFor="need_transport">Need transport? :</label>
+    <input type="checkbox" name="need_transport" id="need_transport" onChange={(e: ChangeEvent<HTMLInputElement>) => setNeedTransport((e.target.checked)) } />
+    { 
+      console.log(needTransport)
+    }
+    {
+      needTransport && <>
+      <label htmlFor="address">Address: </label>
+      <input type="text" id="address" name="address"  />
+      </>
+    }
+    <label htmlFor="equipment-content">Equipment:</label>
+    <textarea name="equipment-content" id="equipment-content" cols={30} rows={10} placeholder="10 XL t-shirts"></textarea>
+  </>)
 }
