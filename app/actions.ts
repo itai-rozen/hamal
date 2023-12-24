ea3bdd01135bc6ddaf7bcf89a3b2ccc8870a5856
itai rozen <itai.rozen@mail.huji.ac.il>
2023-12-24 23:42:16 +0200

    started working on the query function

--- app/actions.ts
+++ app/actions.ts
@@ -1,6 +1,13 @@
 import { sql } from '@vercel/postgres'
 
 export async function createQuery(formData: FormData) {
-  console.log('form data: ', formData)
-  // console.log('detailed form data: ',[...formData.keys()])
+  // console.log('form data: ', formData)
+  console.log('form data keys: ',{...formData.keys()})
+  console.log('form data values: ',{...formData.values()})
+  for (const value in formData.values()) {
+    console.log('value: ', value)
+  }
+  const tableName = formData.get('table-name');
+  const query = `insert into ${tableName}`
+  console.log('query: ', query)
 }
