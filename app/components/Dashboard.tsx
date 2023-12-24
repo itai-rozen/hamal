b4c52de025b790d261d75b06080dc46e3701e1c2
itai rozen <itai.rozen@mail.huji.ac.il>
2023-12-24 23:25:01 +0200

    removed test data

--- app/components/Dashboard.tsx
+++ app/components/Dashboard.tsx
@@ -6,20 +6,11 @@ import styles from './page.module.css'
 
 
 
-export default function Dashboard({  users  }: any) {
+export default function Dashboard() {
   return (
     <main>
       <h1>Hamal</h1>
     <MainForm />
-    {
-      users.map((user: { user_id: number, password: string, email: string }) => {
-        return <div key={user.user_id}>
-            <div>{user.user_id}</div>
-            <div>{user.password}</div>
-            <div>{user.email}</div>
-         </div>
-      })
-    }
     </main>
   )
 }
