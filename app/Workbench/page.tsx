c171f4a8aec61f8b26640c26edd1ae2858d95135
itai rozen <itai.rozen@mail.huji.ac.il>
2023-12-24 23:26:38 +0200

    small style for convenience

--- app/Workbench/page.tsx
+++ app/Workbench/page.tsx
@@ -1,17 +1,18 @@
 "use client"
 import connectDb  from './connect.js';
+import './workbench.css'
 import { ChangeEvent, useState } from 'react'
 export default function Workbench({getQuery} : {getQuery: Function}): React.ReactNode {
   const [query, setQuery] = useState<string>('');
-  const [rows, setRows] = useState<object[]>([]);
+  const [rows, setRows] = useState<object[] | []>([]);
   const [error, setError] = useState<string>('');
   const handleQuery = async () => {
     try {
       const resText: string = await connectDb(query);
-      const res:{rows?:object[], err?: string } = JSON.parse(resText)
-      console.log('res text: ', res)
-      if (res.rows) {
-        setRows(res.rows?.rows)
+      const res:{rows:[], err: string } = JSON.parse(resText)
+      if (res.rows?.length > 0) {
+        console.log('rows" ',res.rows)
+        setRows(res.rows)
       }
       if (res.err) {
         setError(res.err)
@@ -24,14 +25,23 @@ export default function Workbench({getQuery} : {getQuery: Function}): React.Reac
   return (
     <>
       <h1>Workbench</h1>
-      <input type="text" onChange={(e: ChangeEvent<HTMLInputElement>) => setQuery(e.target.value)} />
-      <button onClick={handleQuery}>ok</button>
+      <textarea cols={100} rows={10} onChange={(e: ChangeEvent<HTMLTextAreaElement>) => setQuery(e.target.value)} ></textarea>
+      <button className='sql-btn' onClick={handleQuery}>ok</button>
       {
         error && <h4>{JSON.stringify(error)}</h4>
       }
+      <div className="sql-table">
       {
-        rows.length > 0 && rows.map(row => row?.user_id)
+        rows.length > 0 && rows.map((row:{},i:number) => {
+          return Object.entries(row).map(([key, value]) => {
+            return <div className='sql-column' key={key+value+i}>
+              <p>{key}</p>
+              <p>{value as string}</p>
+            </div>
+          })
+        })
       }
+      </div>
     </>
   )
 } 
