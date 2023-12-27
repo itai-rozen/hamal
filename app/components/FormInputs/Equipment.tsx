c09b91980366c28bbc5e11da88d64ef3e6fcd5a6
itai rozen <itai.rozen@mail.huji.ac.il>
2023-12-27 21:16:50 +0200

    switched to default values

--- app/components/FormInputs/Equipment.tsx
+++ app/components/FormInputs/Equipment.tsx
@@ -5,20 +5,21 @@ export default function Equipment() {
   return (
   <>
     <label htmlFor="fullname">Full Name:</label>
-    <input id="fullname" name="fullname" type="text" required value="test NAME" />
+    <input id="fullname" name="fullname" type="text" required defaultValue="test NAME" />
     <label htmlFor="phone">Phone:</label>
-    <input id="phone" name="phone" type="phone" required value="111111111" />
+    <input id="phone" name="phone" type="phone" required defaultValue="111111111" />
     <label htmlFor="need_transport">Need transport? :</label>
-    <input type="checkbox" name="need_transport" id="need_transport" checked={needTransport} onChange={(e: ChangeEvent<HTMLInputElement>) => setNeedTransport((e.target.checked)) } value={`${needTransport}`} />
+    <input type="checkbox" name="need_transport" id="need_transport" checked={needTransport} onChange={(e: ChangeEvent<HTMLInputElement>) => setNeedTransport((e.target.checked)) } defaultValue={`${needTransport}`} />
     {
       needTransport && <>
       <label htmlFor="address">Address: </label>
-      <input type="text" id="address" name="address" value="test ADDRESS"  />
+      <input type="text" id="address" name="address" defaultValue="test ADDRESS"  />
       </>
     }
     <label htmlFor="equipment-content">Equipment:</label>
-    <textarea name="content" id="equipment-content" cols={30} rows={10} placeholder="10 XL t-shirts">
-      11 sponges
+    <textarea name="content" id="equipment-content" cols={30} rows={10} placeholder="10 XL t-shirts" defaultValue={'11 sponges'}>
+      
     </textarea>
+    <input type="hidden" name="sql_action" id="sql_action" defaultValue="INSERT INTO"/>
   </>)
 }
