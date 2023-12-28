02353c122a10439dda5b251c5d50dc8c93e527b7
itai rozen <itai.rozen@mail.huji.ac.il>
2023-12-28 18:54:40 +0200

    middleware works in api endpoint

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,14 @@ import { getCookie, setCookie } from './actions'
 import { RequestCookie } from 'next/dist/compiled/@edge-runtime/cookies'
 import { cookies } from 'next/headers'
 import { userIdType } from './types/intefaces'
+import { NextResponse } from 'next/server'
 export default async function Home():Promise<JSX.Element>{
-  await fetch('http://localhost:3000/setCookie')
-  const userId = await getCookie('hamal_user_id');
-
+  const response: Response = await fetch('http://localhost:3000/api/setCookie');
+  // console.log('response: ', response)
+  const userId : RequestCookie  |undefined = await getCookie('hamal_user_id');
+  console.log('user id: ', userId?.value)
   return (
-    <Dashboard userId={userId} />
+    <Dashboard userId={userId?.value} />
   )
 }
 
--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,16 @@
 import { NextRequest, NextResponse } from "next/server";
 export function middleware(req: NextRequest) {
   const response = NextResponse.next();
-  if (req.nextUrl.pathname === '/setCookie') {
+  if (req.nextUrl.pathname === '/api/setCookie') {
     console.log('path')
     if (!req.cookies.has('itai')){
       console.log('hasnt')
-      response.cookies.set('itai', 'hi');
-      response.cookies.set('hamal_user_id', '1', { expires: Date.now()+10000, path: '/' });    
+      try {
+        response.cookies.set('itai', 'hi');
+        response.cookies.set('hamal_user_id', '1', { expires: Date.now()+10000 });    
+      } catch(err) {
+        console.log('error setting cookies: ', err)
+      }
     }
   }
   return response;
