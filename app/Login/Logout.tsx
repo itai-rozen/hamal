9d5946534f7dec359e2161062fb98eab30de9bdb
itai rozen <itai.rozen@mail.huji.ac.il>
2024-01-01 16:35:25 +0200

    added logout btn

---
+++ app/Login/Logout.tsx
@@ -0,0 +1,10 @@
+import { signOut } from "next-auth/react"
+const Logout = () => {
+  return (
+    <>
+    <button onClick={() => signOut()}>Logout</button>
+    </>
+  )
+}
+
+export default Logout
