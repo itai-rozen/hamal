ca53c1e14cfc5c3983b9a4c0957df1e7e8eeda9f
itai rozen <itai.rozen@mail.huji.ac.il>
2024-01-01 16:35:38 +0200

    added login page to nav

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,8 +1,10 @@
 import Link from "next/link"
+import { NextAuthProvider } from "./AuthProvider";
 const Nav = () => {
   return <nav>
     <Link href="/">Dashboard</Link>
     <Link href="/Workbench">Workbench</Link>
+    <Link href="/Login">Login</Link>
 
   </nav>
 }
