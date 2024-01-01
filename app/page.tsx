60af1b677d1f4c4e9406d9e036930e3b70f76070
itai rozen <itai.rozen@mail.huji.ac.il>
2024-01-01 16:33:18 +0200

    removed old stuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,8 @@
-import Image from 'next/image'
-import styles from './page.module.css'
 import Dashboard from './components/Dashboard'
 import { getCookie, setCookie } from './actions'
 import { RequestCookie } from 'next/dist/compiled/@edge-runtime/cookies'
-import { cookies } from 'next/headers'
-import { userIdType } from './types/intefaces'
-import { NextResponse } from 'next/server'
 export default async function Home():Promise<JSX.Element>{
-  const response: Response = await fetch('http://localhost:3000/api/setCookie');
-  // console.log('response: ', response)
-  const userId : RequestCookie  |undefined = await getCookie('hamal_user_id');
+  const userId : RequestCookie | undefined = await getCookie('hamal_user_id');
   console.log('user id: ', userId?.value)
   return (
     <Dashboard userId={userId?.value} />
