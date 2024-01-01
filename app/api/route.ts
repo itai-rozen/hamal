e1764c62b7d34480897c521dbe8477170b038c75
itai rozen <itai.rozen@mail.huji.ac.il>
2024-01-01 16:33:45 +0200

    added routehandler for next auth

--- app/api/route.ts
+++
@@ -1,9 +0,0 @@
-"use server"
-import { RequestCookie } from "next/dist/compiled/@edge-runtime/cookies";
-import { cookies } from "next/headers";
-import { NextResponse } from "next/server";
-export async function GET() {
-  // await cookies().set('itai', 'hi');
-  // const cks : RequestCookie|RequestCookie[]|undefined =  cookies().get('itai')   
-  // return NextResponse.json(cks);
-}
