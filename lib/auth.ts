259eda585c9d22801a25ef29dced7d09bd3ca0f4
itai rozen <itai.rozen@mail.huji.ac.il>
2024-01-02 14:13:04 +0200

    added credential provider

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,14 +1,34 @@
 import { NextAuthOptions } from "next-auth";
 import NextAuth from "next-auth/next";
 import GoogleProvider from 'next-auth/providers/google'
+import CredentialsProvider from "next-auth/providers/credentials";
 
-export const authOptions:NextAuthOptions = {
+interface Credentials extends Record<"email" | "password", string> {}
+
+export const authOptions : NextAuthOptions = {
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
+    CredentialsProvider({
+      name: 'Sign in',
+      credentials: {
+        email: { label: 'Email', type: 'email', placeholder: 'example@exampke.com'},
+        password: { label: "Password", type: "password" }
+      },
+      /* eslint-disable-next-line @typescript-eslint/ban-ts-comment*/
+      // @ts-ignore
+      async authorize(credentials) {
+        console.log('credentials: ', credentials)
+        const user : {email: string, password: string} = {email: '2@ee', password: '12345'};
+        if (user) 
+          return user;
+        else
+          return null;
+      }
+    })
     // ...add more providers here
   ]
 }
