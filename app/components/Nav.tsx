f722770236e916d45b6668e45ebc0983f5c56822
itai rozen <itai.rozen@mail.huji.ac.il>
2024-01-03 20:53:12 +0200

    added some tailwind classes

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,12 +1,16 @@
 import Link from "next/link"
-import { NextAuthProvider } from "./AuthProvider";
 const Nav = () => {
-  return <nav>
-    <Link href="/">Dashboard</Link>
-    <Link href="/Workbench">Workbench</Link>
-    <Link href="/Login">Login</Link>
-
-  </nav>
+  return (
+    <nav 
+      className="bg-red-400 h-10 max-h-10 min-h-6 flex justify-between px-6 align-bottom items-center 
+                   font-bold group
+                   *: text-slate-200 *: text-[16px] *:transition-all">
+        <Link className="hover:text-white   hover:transition-all hover:transition-in" href="/">Dashboard</Link>
+        <Link href="/TableManager">Table Manager</Link>
+        <Link href="/Workbench">Workbench</Link>
+        <Link href="/Login">Login</Link>
+    </nav>
+  )
 }
 
 export default Nav;
