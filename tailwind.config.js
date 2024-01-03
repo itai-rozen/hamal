f8bfac33b897489043946c31559ed21dc9ab7bb5
itai rozen <itai.rozen@mail.huji.ac.il>
2024-01-03 20:52:55 +0200

    installed tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.tsx', // Note the addition of the `app` directory.
+    './app/*.tsx', // Note the addition of the `app` directory.
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
