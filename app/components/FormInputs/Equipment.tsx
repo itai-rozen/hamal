3195c8f7a57351f1cb89c69d8e52d9f0eae5d39e
itai rozen <itai.rozen@mail.huji.ac.il>
2024-01-04 21:45:47 +0200

    replaced inputs with styled components

--- app/components/FormInputs/Equipment.tsx
+++ app/components/FormInputs/Equipment.tsx
@@ -1,25 +1,70 @@
 import { ChangeEvent, useState } from "react"
+import { Checkbox, FormControlLabel, Input, TextField } from "@mui/material"
 
 export default function Equipment() {
   const [needTransport, setNeedTransport] = useState<boolean>(true)
   return (
   <>
-    <label htmlFor="fullname">Full Name:</label>
-    <input id="fullname" name="fullname" type="text" required defaultValue="test NAME" />
-    <label htmlFor="phone">Phone:</label>
-    <input id="phone" name="phone" type="phone" required defaultValue="111111111" />
-    <label htmlFor="need_transport">Need transport? :</label>
-    <input type="checkbox" name="need_transport" id="need_transport" checked={needTransport} onChange={(e: ChangeEvent<HTMLInputElement>) => setNeedTransport((e.target.checked)) } defaultValue={`${needTransport}`} />
+    <TextField
+      sx={{my: 5}}
+      id="fullname" 
+      name="fullname" 
+      variant="standard" 
+      label="Full Name" 
+      required 
+      value={"test NAME"}  
+       />
+    <TextField 
+      id="phone" 
+      name="phone" 
+      type="phone" 
+      variant="standard" 
+      label="Phone" 
+      required 
+      value={"111111111"}  
+    />
+    <FormControlLabel 
+      control={
+        <Checkbox 
+          defaultChecked 
+          checked={needTransport}
+          name="need_transport" 
+          value={needTransport}
+          onChange={(e: ChangeEvent<HTMLInputElement>) => setNeedTransport((e.target.checked)) } 
+        />} 
+      label="Need transport?" 
+    />
     {
-      needTransport && <>
-      <label htmlFor="address">Address: </label>
-      <input type="text" id="address" name="address" defaultValue="test ADDRESS"  />
-      </>
+      needTransport && 
+      <TextField 
+        sx={{my:5}}
+        label='Address'
+        name="address"
+        defaultValue={"test ADDRESS"}
+      />
     }
-    <label htmlFor="equipment-content">Equipment:</label>
-    <textarea name="content" id="equipment-content" cols={30} rows={10} placeholder="10 XL t-shirts" defaultValue={'11 sponges'}>
-      
-    </textarea>
+    <TextField
+      sx={{my: 5}}
+      variant="outlined"
+      fullWidth
+      multiline
+      minRows={10}
+      label="Equipment"
+      name="content" 
+      id="equipment-content"
+      placeholder="10 XL t-shirts"
+      value="11 octopus"
+      />
+    <TextField
+      variant="outlined"
+      fullWidth
+      multiline
+      minRows={10}
+      label="notes"
+      name="notes" 
+      id="equipment-notes"
+      placeholder="Anything else you want to add?"
+      />
     <input type="hidden" name="sql_action" id="sql_action" defaultValue="INSERT INTO"/>
   </>)
 }
