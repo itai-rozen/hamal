498fd16bdc8748cb0afdc5adc34044776ecaa0fb
itai rozen <itai.rozen@mail.huji.ac.il>
2024-01-08 20:05:29 +0200

    switched to nextauth middleware

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,20 @@
-import { NextRequest, NextResponse } from "next/server";
-export function middleware(req: NextRequest) {
-  const response = NextResponse.next();
-  if (req.nextUrl.pathname === '/api/setCookie') {
-    console.log('path')
-    if (!req.cookies.has('itai')){
-      console.log('hasnt')
-      try {
-        response.cookies.set('itai', 'hi');
-        response.cookies.set('hamal_user_id', '1', { expires: Date.now()+10000 });    
-      } catch(err) {
-        console.log('error setting cookies: ', err)
-      }
-    }
-  }
-  return response;
-}
+export { default } from "next-auth/middleware"
+export const config = { matcher: ["/TableManager", '/Workbench'] }
+// import { NextRequest, NextResponse } from "next/server";
+// export function middleware(req: NextRequest) {
+//   const response = NextResponse.next();
+  
+//   if (req.nextUrl.pathname === '/api/setCookie') {
+//     console.log('path')
+//     if (!req.cookies.has('itai')){
+//       console.log('hasnt')
+//       try {
+//         response.cookies.set('itai', 'hi');
+//         response.cookies.set('hamal_user_id', '1', { expires: Date.now()+10000 });    
+//       } catch(err) {
+//         console.log('error setting cookies: ', err)
+//       }
+//     }
+//   }
+//   return response;
+// }
