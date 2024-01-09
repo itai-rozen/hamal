f00861a43b1ba309aedf525993388b7c4fd975ea
itai rozen <itai.rozen@mail.huji.ac.il>
2024-01-09 03:31:58 +0200

    if logged user is not a manager dont show manager nav items

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,14 +1,19 @@
+import { Session } from "next-auth";
 import Link from "next/link"
-const Nav = () => {
+const Nav = ({ session, isManager }: { session: Session | null, isManager: boolean }) => {
   return (
-    <nav 
+    <nav
       className="bg-red-400 h-10 max-h-10 min-h-6 flex justify-between px-6 align-bottom items-center 
                    font-bold group
                    *: text-slate-200 *: text-[16px] *:transition-all">
-        <Link className="hover:text-white   hover:transition-all hover:transition-in" href="/">Dashboard</Link>
-        <Link className="hover:text-white   hover:transition-all hover:transition-in" href="/TableManager">Table Manager</Link>
-        <Link className="hover:text-white   hover:transition-all hover:transition-in" href="/Workbench">Workbench</Link>
-        <Link className="hover:text-white   hover:transition-all hover:transition-in" href="/Login">Login</Link>
+      <Link className="hover:text-white   hover:transition-all hover:transition-in" href="/">Dashboard</Link>
+      {
+        isManager && <>
+          <Link className="hover:text-white   hover:transition-all hover:transition-in" href="/TableManager">Table Manager</Link>
+          <Link className="hover:text-white   hover:transition-all hover:transition-in" href="/Workbench">Workbench</Link>
+        </>
+      }
+      <Link className="hover:text-white   hover:transition-all hover:transition-in" href="/Login">Login</Link>
     </nav>
   )
 }
