6834e9213f7b518b70e0e6d69ec628046c16b607
itai rozen <itai.rozen@mail.huji.ac.il>
2024-01-10 23:03:18 +0200

    removed msg & added more styles

--- app/Login/login.tsx
+++ app/Login/login.tsx
@@ -4,20 +4,12 @@ import { signIn } from "next-auth/react"
 import { useState } from "react"
 import { connectDb } from "../actions"
 import bcrypt from 'bcryptjs'
-import { Button, TextField } from "@mui/material"
+import { Button, Link, TextField, Typography } from "@mui/material"
+import { handleSignin, signUp } from "../actions"
 
 export default function Login() {
-  const [email, setEmail] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
+  const [isAccount, setIsAccount] = useState<boolean>(true);
   const [error, setError] = useState<string>('');
-  const [msg, setMsg] = useState<string>('');
-
-  async function handleLogin() {
-    signIn('google');
-    await fetch('http://localhost:3000/api/setCookie', {
-      method: 'post'
-    })
-  }
 
   async function signUp(formData: FormData) {
     const email = formData.get('signup_email')
@@ -34,84 +26,92 @@ export default function Login() {
     console.log('response: ', rows, err);
     if (err) {
       setError(JSON.stringify(err));
-      setMsg('');
     }
     else {
       setError('');
-      setMsg('success!')
     }
   }
+
   return (
     <>
-      <div className="w-[50vw] mx-auto my-5 *:form:border-black">
+      <div className="w-[50vw] mx-auto my-5 space-y-5   
+                      [&>div]:border-2  [&>div]:rounded-sm
+                      [&_form>*]:mx-4 [&_.sign]:m-4 [&>section]:text-center [&>section]:my-1">
 
-        <Button onClick={() => signIn("google")}>
-          <Image
-            src='/google.svg'
-            alt="google icon"
-            width={100}
-            height={50}
-            priority={true}
+        <div className="flex flex-col items-center">
+
+          <Button className="w-[100%]" onClick={() => signIn("google")}>
+            <Image
+              src='/google.svg'
+              alt="google icon"
+              width={50}
+              height={30}
+              priority={true}
             />
-            <h2>Login with google</h2>
-        </Button>
+            <h2 className="mx-2">Sign in with google</h2>
+          </Button>
+        </div>
 
-        <div className="my-2 text-center">OR</div>
-        
-        <form className="flex flex-col  [&>div]:my-4 *:py-3 " action={() => signIn('credentials', {
-          redirect: false,
-          email,
-          password
-        })}>
-          <h2>Login with email & password</h2>
-          <TextField
-            id="email"
-            name="email"
-            label="Email"
-            onChange={e => setEmail(e.target.value)}
-            type="email"
-          />
-          <TextField
-            id="password"
-            name="password"
-            label="Password"
-            onChange={e => setPassword(e.target.value)}
-            type="password"
-          />
-          <Button type="submit">Submit</Button>
-        </form>
-        <div className="my-2">OR</div>
-        <form action={signUp} className="flex flex-col  [&>div]:my-4 *:py-3">
-          <h2>Sign Up</h2>
-          <TextField
-            id="signup_email"
-            name="signup_email"
-            type="email"
-            label="Email"
-          />
-          <small className="text-red-500">
-           * password must contain at least one number and one letter, and at least 8 or more characters
-          </small>
-          <TextField
-            id="signup_password"
-            name="signup_password"
-            type="password"
-            label="Password"
-            inputProps={{pattern: "(?=.*\d)(?=.*[a-z]).{8,}" }}
-            title="Must contain at least one number and one letter, and at least 8 or more characters"
-          />
-                   <TextField
-            id="signup_repassword"
-            name="signup_repassword"
-            type="password"
-            label="Re-enter password"
-            inputProps={{pattern: "(?=.*\d)(?=.*[a-z]).{8,}" }}
-            title="Must contain at least one number and one letter, and at least 8 or more characters"
-          />
-          <Button type="submit">Submit</Button>
-        </form>
-        {error && <p>{error}</p>}
-        {msg && <p>{msg}</p>}
+        <section>OR</section>
+       {
+        isAccount ? <div>
+          <form className="flex flex-col  space-y-4 *:py-3 " action={handleSignin}>
+            <Typography variant="h6" className="text-center">Sign in with email & password</Typography>
+            <TextField
+              id="email"
+              name="email"
+              label="Email"
+              type="email"
+            />
+            <TextField
+              id="password"
+              name="password"
+              label="Password"
+              type="password"
+            />
+            <Button type="submit">Sign In</Button>
+          </form>
+          <Typography className="sign">dont have an account? <Link href="#" onClick={() => setIsAccount(false)}>sign up</Link></Typography>
+             
+        </div>
+        : 
+        <div>
+          <form action={signUp} className="flex flex-col  space-y-4 *:py-3">
+            <Typography variant="h6" className="text-center">Sign Up</Typography>
+            <TextField
+              id="signup_email"
+              name="signup_email"
+              type="email"
+              label="Email"
+            />
+            <small className="text-red-500">
+              * password must contain at least one number and one letter, and at least 8 or more characters
+            </small>
+            <TextField
+              id="signup_password"
+              name="signup_password"
+              type="password"
+              label="Password"
+              // inputProps={{ pattern: "^(?=.*\d).{8,}$" }}
+              inputProps={{ pattern: "(?=.*\d)(?=.*[a-zA-Z]).{8,}$" }}
+              title="Must contain at least one number and one letter, and at least 8 or more characters"
+            />
+            <TextField
+              id="signup_repassword"
+              name="signup_repassword"
+              type="password"
+              label="Re-enter password"
+              // inputProps={{ pattern: "^(?=.*\d).{8,}$" }}
+              inputProps={{ pattern:"[a-z]" }}
+              title="Must contain at least one number and one letter, and at least 8 or more characters"
+            />
+            <Button type="submit">Create an Account</Button>
+          </form>
+          <Typography className="sign">already have an account? <Link href="#" onClick={() => setIsAccount(true)}>sign in</Link></Typography>
+
+        </div>
+       } 
+        {error && <p className="text-red-500 text-center">{error}</p>}
       </div>
 
     </>
