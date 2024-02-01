174315daabdb266a700549461d3919aa5d3595f0
itai rozen <itai.rozen@mail.huji.ac.il>
2024-02-01 11:59:24 +0200

    added transport table schema

--- app/schema/schema.sql
+++ app/schema/schema.sql
@@ -11,3 +11,17 @@ CREATE TABLE IF NOT EXISTS equipment (
 
 CREATE INDEX IF NOT EXISTS equipment_phone_key ON equipment(phone);
 ALTER TABLE equipment ADD COLUMN status status DEFAULT 'pending'
+
+CREATE TABLE IF NOT EXISTS transport (
+  id SERIAL PRIMARY KEY,
+  fullname VARCHAR(120) NOT NULL, 
+  phone VARCHAR(15) NOT NULL,
+  address_from VARCHAR(255) NOT NULL,
+  address_to VARCHAR(255) NOT NULL,
+  content VARCHAR(1000) DEFAULT NULL,
+  status status DEFAULT 'pending',
+  notes VARCHAR(1000) NOT NULL,
+  date_created TIMESTAMP DEFAULT NOW(),
+  date_modified TIMESTAMP DEFAULT NOW()
+)
+CREATE INDEX IF NOT EXISTS transport ON transport(phone);
